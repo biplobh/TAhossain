87fc4e82d77ba3517614a250ef07af376659ef1f
biplobh <mdbhossain999@gmail.com>
2023-01-24 13:26:23 -0500

    This is testing

---
+++ src/Item.java
@@ -0,0 +1,19 @@
+public class Item {
+
+    protected String name;
+
+    protected String desc;
+
+    protected int value;
+
+    public Item(String name, String desc, int value){
+    //Item i1 = new Item(name="Item name", desc="Item desc", value = 5);
+
+        this.name = name;
+        this.desc = desc;
+        this.value = value;
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Item i1 = new Item("Item name", "Item desc",  10);
+        Item i2 = new Item("Item name2", "Item desc2", 11);
+        //System.out.println("Hello world!");
+    }
+}
